69dc30bed858dc24e5687257acd1e3bdd0b281ab
Felipe C. Araujo <felipe.s-04@hotmail.com>
2021-04-21 20:04:57 -0300

    add Settings controller

--- app/app.ts
+++ app/app.ts
@@ -1,12 +1,12 @@
-import 'reflect-metadata';
 import express from 'express';
-import "express-async-errors";
-
 import cors from 'cors';
+import "../src/database";
+import { routes } from '../src/routes';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(routes);
 
 export { app };
--- bin/server.ts
+++ bin/server.ts
@@ -1,8 +1,55 @@
-import express from 'express'
-import '../src/database'
+import http from 'http';
+import { app } from '../app/app';
 
-const app = express();
+const port = normalizePort(process.env.PORT || '3000');
 
-app.listen(3333, () => {
-    console.log('Servidor no ar!')
-})
+const server = http.createServer(app);
+
+app.set('port', port);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+console.log("Running Server in Port: " + port);
+
+function normalizePort(val: any) {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
+}
+
+function onError(error: any) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    const bind = typeof port === 'string' ?
+        'Pipe ' + port :
+        'Port ' + port;
+
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');;
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+function onListening() {
+    const addr: any = server.address();
+    const bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+}
