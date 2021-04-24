a5837a640e07418851c47f0e6e82c3d7baf63a64
Felipe C. Araujo <felipe.s-04@hotmail.com>
2021-04-23 21:15:55 -0300

    add socket.io

--- app/app.ts
+++ app/app.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import { createServer } from "http"
+import { Server, Socket } from "socket.io";
 import cors from 'cors';
 import "../src/database";
 import { routes } from '../src/routes';
 
 const app = express();
+
+const http = createServer(app);
+const io = new Server(http);
 
 app.use(cors());
 app.use(express.json());
